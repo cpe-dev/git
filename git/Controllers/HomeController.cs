9856b791a927a49fcc197dd44faac7b250afc5a0
ratchawut <ratchawutk55@email.nu.ac.th>
2017-03-02 16:26:14 +0700

    init EF

--- git/Controllers/HomeController.cs
+++ git/Controllers/HomeController.cs
@@ -8,11 +8,16 @@ namespace git.Controllers
 {
     public class HomeController : Controller
     {
+        TOPEntities db = new TOPEntities();
         //
         // GET: /Home/
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Create()
+        {
+            return View(db.Blogs.ToList());
         }
 	}
 }
